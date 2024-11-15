ec895e08e34176c2966ed7f969dba4bbc449e530
Prashant Palve <palveprashant526@gmail.com>
2024-11-15 23:56:37 +0530

    some change in movie and movie detail page

--- public/js/movie_detail.js
+++ public/js/movie_detail.js
@@ -21,7 +21,7 @@ async function fetchMovieData() {
 }
 
 function displayMovies(movie) {
-    document.querySelector(".container").style.backgroundImage = `url(${movie.banner_image})`;
+    document.querySelector(".trailer-section iframe").src = `${movie.trailer}`;
     document.querySelector(".poster img").src = movie.poster;
     document.querySelector(".movie-title").textContent = movie.movie_title;
     document.querySelector(".movie-rating").textContent = `${movie.rating}/10`;
@@ -29,37 +29,13 @@ function displayMovies(movie) {
     document.querySelector(".movie-language").textContent = `${movie.language}`;
     document.querySelector(".movie-details").textContent = `${movie.duration} mins • ${movie.genre} • ${movie.language} • ${movie.release_date}`;
     document.querySelector(".movie-synopsis").textContent = movie.synopsis;
-
-    const castContainer = document.querySelector(".cast-section");
-    castContainer.innerHTML = ""; 
-    movie.cast.forEach(cast => {
-        const castHTML = `
-            <div class="text-center">
-                <img src="${cast.image}" alt="${cast.actor}" class="w-24 h-24 rounded-full mx-auto shadow-lg">
-                <p class="text-lg font-semibold text-gray-800 mt-2">${cast.actor}</p>
-                <p class="text-gray-500">${cast.role}</p>
-            </div>
-        `;
-        castContainer.insertAdjacentHTML("beforeend", castHTML);
-    });
-
-    const crewContainer = document.querySelector(".crew-section");
-    crewContainer.innerHTML = ""; 
-    movie.crew.forEach(crew => {
-        const crewHTML = `
-            <div class="text-center">
-                <img src="${crew.image}" alt="${crew.name}" class="w-24 h-24 rounded-full mx-auto shadow-lg">
-                <p class="text-lg font-semibold text-gray-800 mt-2">${crew.name}</p>
-                <p class="text-gray-500">${crew.role}</p>
-            </div>
-        `;
-        crewContainer.insertAdjacentHTML("beforeend", crewHTML);
-    });
 }
 
 
 document.getElementById("book_ticket").addEventListener("click", ()=>{
     alert(movieId)
 })
+
+
 
 fetchMovieData();
